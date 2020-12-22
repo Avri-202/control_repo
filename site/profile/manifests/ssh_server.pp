a66069b43ccbe836b3dbb9a81e181ce563c381e9
Avri-202 <76390984+Avri-202@users.noreply.github.com>
2020-12-22 18:27:42 +0200

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQD0JXPUmMcidBZNU2iwJ3StdAfoIaQ5QSNfAJQa4VnRot3jP4ffFZzKFa/zFp14v5lqTdz/asVJHNIEONI7RxeHrd0FiyqSTEiyKaalj6v2xt/NEw3tYSMfsbkKTbDZoHwML6PYdxlED9qRelPUNCX6Lruotl3TfPmj//fRsESGPL0HPhGF8Yq78h/dcUcnXh+VmJcO3UlsOYiMEeu5tSmBkT+ZBQjXURImgG7+vLedP/zJcKuwZW1/Dr9n6Il+b/717OlyqhdE8yKejKkO/raZNnfdB63WSHUqjujFd93NBYxvcqHlZjyUk22ZI+D3262Hep9/mjZyimtqHPeVbF+D',
   }  
 }
